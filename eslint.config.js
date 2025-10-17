e8a26c50f15eed884581879ac1f42e1cdd78053d
meetqy <meetqy@icloud.com>
2025-10-17 16:03:16 +0800

    eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,9 @@
-import { FlatCompat } from "@eslint/eslintrc";
 import tseslint from "typescript-eslint";
-
-const compat = new FlatCompat({
-  baseDirectory: import.meta.dirname,
-});
 
 export default tseslint.config(
   {
-    ignores: [".next", "*.js"],
+    ignores: [".next", "*.js", "next-env.d.ts"],
   },
-  ...compat.extends("next/core-web-vitals"),
   {
     files: ["**/*.ts", "**/*.tsx"],
     extends: [...tseslint.configs.recommended, ...tseslint.configs.recommendedTypeChecked, ...tseslint.configs.stylisticTypeChecked],
